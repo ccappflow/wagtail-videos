e2f85a905b911fc65461888a6965d559ea4aa0a9
Seb Brown <seb@neonjungle.studio>
2026-06-04 08:53:25 +1000

    Also remove chunk from here

--- wagtailvideos/views/videos.py
+++ wagtailvideos/views/videos.py
@@ -11,7 +11,6 @@ from wagtail.admin.views import generic
 from wagtail.search.backends import get_search_backends
 
 from wagtailvideos import get_transcoder_backend, get_video_model
-from wagtailvideos.chunked_uploads import handle_chunked_upload
 from wagtailvideos.forms import VideoTranscodeAdminForm, get_video_form
 from wagtailvideos.permissions import permission_policy
 
@@ -34,14 +33,14 @@ class IndexView(generic.IndexView):
     model = Video
     filterset_class = VideoFilterSet
     permission_policy = permission_policy
-    any_permission_required = ["add", "change", "delete"]
-    header_icon = "media"
-    template_name = "wagtailvideos/videos/index.html"
-    results_template_name = "wagtailvideos/videos/results.html"
+    any_permission_required = ['add', 'change', 'delete']
+    header_icon = 'media'
+    template_name = 'wagtailvideos/videos/index.html'
+    results_template_name = 'wagtailvideos/videos/results.html'
     _show_breadcrumbs = True
     add_url_name = "wagtailvideos:add_multiple"
     edit_url_name = "wagtailvideos:edit"
-    index_results_url_name = "wagtailvideos:index_results"
+    index_results_url_name = 'wagtailvideos:index_results'
     add_item_label = "Add a video"
 
     def get_breadcrumbs_items(self):
@@ -58,7 +57,7 @@ class IndexView(generic.IndexView):
         return 32  # 4 x 8
 
 
-@permission_checker.require("change")
+@permission_checker.require('change')
 def edit(request, video_id):
     Video = get_video_model()
     VideoForm = get_video_form(Video)
@@ -68,7 +67,7 @@ def edit(request, video_id):
         original_file = video.file
         form = VideoForm(request.POST, request.FILES, instance=video)
         if form.is_valid():
-            if "file" in form.changed_data:
+            if 'file' in form.changed_data:
                 # if providing a new video file, delete the old one and all renditions.
                 # NB Doing this via original_file.delete() clears the file field,
                 # which definitely isn't what we want...
@@ -90,45 +89,29 @@ def edit(request, video_id):
     else:
         form = VideoForm(instance=video)
 
-    if not video._meta.get_field("file").storage.exists(video.file.name):
+    if not video._meta.get_field('file').storage.exists(video.file.name):
         # Give error if video file doesn't exist
-        messages.error(
-            request,
-            _(
-                "The source video file could not be found. Please change the source or delete the video."
-            ).format(video.title),
-            buttons=[
-                messages.button(
-                    reverse("wagtailvideos:delete", args=(video.id,)), _("Delete")
-                )
-            ],
-        )
-    if hasattr(video, "track_listing"):
-        action_url = reverse(
-            "wagtailvideos_tracks:edit", args=(video.track_listing.pk,)
-        )
+        messages.error(request, _(
+            "The source video file could not be found. Please change the source or delete the video."
+        ).format(video.title), buttons=[
+            messages.button(reverse('wagtailvideos:delete', args=(video.id,)), _('Delete'))
+        ])
+    if hasattr(video, 'track_listing'):
+        action_url = reverse('wagtailvideos_tracks:edit', args=(video.track_listing.pk,))
     else:
-        action_url = reverse("wagtailvideos_tracks:add")
+        action_url = reverse('wagtailvideos_tracks:add')
 
-    return render(
-        request,
-        "wagtailvideos/videos/edit.html",
-        {
-            "video": video,
-            "form": form,
-            "filesize": video.get_file_size(),
-            "transcoder_installed": get_transcoder_backend().installed(),
-            "transcoder_enabled": not getattr(
-                settings, "WAGTAIL_VIDEOS_DISABLE_TRANSCODE", False
-            ),
-            "transcodes": video.transcodes.all(),
-            "transcode_form": VideoTranscodeAdminForm(video=video),
-            "tracks_action_url": action_url,
-            "user_can_delete": permission_policy.user_has_permission_for_instance(
-                request.user, "delete", video
-            ),
-        },
-    )
+    return render(request, "wagtailvideos/videos/edit.html", {
+        'video': video,
+        'form': form,
+        'filesize': video.get_file_size(),
+        'transcoder_installed': get_transcoder_backend().installed(),
+        'transcoder_enabled': not getattr(settings, 'WAGTAIL_VIDEOS_DISABLE_TRANSCODE', False),
+        'transcodes': video.transcodes.all(),
+        'transcode_form': VideoTranscodeAdminForm(video=video),
+        'tracks_action_url': action_url,
+        'user_can_delete': permission_policy.user_has_permission_for_instance(request.user, 'delete', video)
+    })
 
 
 @require_POST
@@ -138,94 +121,69 @@ def create_transcode(request, video_id):
 
     if transcode_form.is_valid():
         transcode_form.save()
-    return redirect("wagtailvideos:edit", video_id)
+    return redirect('wagtailvideos:edit', video_id)
 
 
-@permission_checker.require("delete")
+@permission_checker.require('delete')
 def delete_transcode(request, video_id, transcode_id):
     video = get_object_or_404(get_video_model(), id=video_id)
     video.transcodes.filter(id=transcode_id).delete()
-    return redirect("wagtailvideos:edit", video_id)
+    return redirect('wagtailvideos:edit', video_id)
 
 
-@permission_checker.require("delete")
+@permission_checker.require('delete')
 def delete(request, video_id):
     video = get_object_or_404(get_video_model(), id=video_id)
 
     if request.POST:
         video.delete()
         messages.success(request, _("Video '{0}' deleted.").format(video.title))
-        return redirect("wagtailvideos:index")
+        return redirect('wagtailvideos:index')
 
-    return render(
-        request,
-        "wagtailvideos/videos/confirm_delete.html",
-        {
-            "video": video,
-        },
-    )
+    return render(request, "wagtailvideos/videos/confirm_delete.html", {
+        'video': video,
+    })
 
 
-@permission_checker.require("add")
+@permission_checker.require('add')
 def add(request):
     Video = get_video_model()
     VideoForm = get_video_form(Video)
 
-    chunk_result = handle_chunked_upload(request, field_name="file")
-    if chunk_result.response:
-        return chunk_result.response
-
     if request.POST:
-        try:
-            video = Video(uploaded_by_user=request.user)
-            form = VideoForm(
-                request.POST, request.FILES, instance=video, user=request.user
-            )
-            if form.is_valid():
-                # Save
-                video = form.save(commit=False)
-                video.file_size = video.file.size
-                video.save()
+        video = Video(uploaded_by_user=request.user)
+        form = VideoForm(request.POST, request.FILES, instance=video, user=request.user)
+        if form.is_valid():
+            # Save
+            video = form.save(commit=False)
+            video.file_size = video.file.size
+            video.save()
 
-                # Success! Send back an edit form
-                for backend in get_search_backends():
-                    backend.add(video)
+            # Success! Send back an edit form
+            for backend in get_search_backends():
+                backend.add(video)
 
-                messages.success(
-                    request,
-                    _("Video '{0}' added.").format(video.title),
-                    buttons=[
-                        messages.button(
-                            reverse("wagtailvideos:edit", args=(video.id,)), _("Edit")
-                        )
-                    ],
-                )
-                return redirect("wagtailvideos:index")
-            else:
-                messages.error(
-                    request, _("The video could not be created due to errors.")
-                )
-        finally:
-            if chunk_result.cleanup:
-                chunk_result.cleanup()
+            messages.success(request, _("Video '{0}' added.").format(video.title), buttons=[
+                messages.button(reverse('wagtailvideos:edit', args=(video.id,)), _('Edit'))
+            ])
+            return redirect('wagtailvideos:index')
+        else:
+            messages.error(request, _("The video could not be created due to errors."))
     else:
         form = VideoForm(user=request.user)
 
-    return render(
-        request,
-        "wagtailvideos/videos/add.html",
-        {
-            "form": form,
-        },
-    )
+    return render(request, "wagtailvideos/videos/add.html", {
+        'form': form,
+    })
 
 
 def usage(request, video_id):
     video = get_object_or_404(get_video_model(), id=video_id)
 
     paginator = Paginator(video.get_usage(), per_page=12)
-    page = paginator.get_page(request.GET.get("p"))
+    page = paginator.get_page(request.GET.get('p'))
 
-    return render(
-        request, "wagtailvideos/videos/usage.html", {"video": video, "used_by": page}
-    )
+    return render(request, "wagtailvideos/videos/usage.html", {
+        'video': video,
+        'used_by': page
+    })
